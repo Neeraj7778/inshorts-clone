20d60da858931fd4ad903ee328d0466eef0eed81
Neeraj <138520151+Neeraj7778@users.noreply.github.com>
2024-08-13 00:00:38 +0530

    Update api.js

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const getNews = async () => {
-    const URL = 'https://newsapi.org/v2/everything?q=apple&from=2024-08-11&to=2024-08-11&sortBy=popularity&apiKey=1630733f01b6491cad00fb67a11bddbd';
+    const URL = 'http://localhost:4000';
     try{
-        return await axios.get(`${URL}`);
+        return await axios.get(`${URL}/news`);
     }
     catch(error){
         console.log('Error while calling get news api ', error);
