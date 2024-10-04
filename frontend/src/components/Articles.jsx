74ded4e846c73c6256cad9ba24521e71452192f8
Neeraj <138520151+Neeraj7778@users.noreply.github.com>
2024-10-04 17:49:19 +0530

    Update Articles.jsx

--- frontend/src/components/Articles.jsx
+++ frontend/src/components/Articles.jsx
@@ -3,36 +3,35 @@ import { useEffect, useState } from "react";
 import { getNews } from "../service/api";
 import Article from "./Article";
 
-
 const Articles = () => {
     
-    const[news, setNews] = useState([]);
+    const [news, setNews] = useState([]);
 
-    useEffect(() =>{
-        dailyNews();
-    },[]);
+    useEffect(() => {
+        if (news.length === 0) {  // Only fetch if news is empty
+            dailyNews();
+        }
+    }, [news]); // Add 'news' as a dependency to avoid unnecessary fetching
 
-    const dailyNews = async() => {
+    const dailyNews = async () => {
         try {
             let response = await getNews();
             setNews(response.data.articles);
-        } 
-        catch (error) {
+        } catch (error) {
             console.log(error);
             console.log(error.message);   
         }
-        
-    }
+    };
 
-    return(
+    return (
         <Box>
-                {
-                    news.map(data => (
-                        <Article data= {data} />
-                    ))
-                }
+            {
+                news.map((data, index) => (
+                    <Article key={index} data={data} />
+                ))
+            }
         </Box>
-    )
-}
+    );
+};
 
 export default Articles;
